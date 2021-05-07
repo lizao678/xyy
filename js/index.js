ba9436d32aded4f23796b3aa0fe10b0f85680e06
ppzhang <mzhangyik@.qq.com>
2021-05-07 16:45:00 +0800

    5.7更新

--- js/index.js
+++ js/index.js
@@ -1,19 +1,26 @@
 window.onload = function() {
     // console.log($(window).width());
     // 当屏幕小于768时默认使用的是移动设备
+    $('.wechat').hide();
+
+    function tog() {
+        $(".wechat").toggle();
+    }
     $(".icon-wechat").on({
         mouseenter:function() {
             if ($(window).width() > 767) {
-               $(".icon-wechat > span").addClass('wechat');
+               $(".wechat").show();
             }
         },
         mouseleave:function() {
             if ($(window).width() > 767) {
-               $(".icon-wechat > span").removeClass('wechat');
+               $(".wechat").hide();
             }
         },
-        touchstart:function() {
-           $(".icon-wechat > span").toggleClass('wechat');
-        }
-    })
+        touchstart:tog
+    });
+    $('.wechat').on('touchstart',function(e) {
+        e.stopPropagation();
+    });
+
 }
